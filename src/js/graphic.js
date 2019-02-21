8ecf754405cc3c0e9c0b10b4b6c2c23ff8354782
Jan Diehm <jandiehm@Jans-MBP-2.lan1>
2019-02-21 11:10:07 -0600

    first pass at mobile styling

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -3,7 +3,21 @@
 function resize() {}
 
 function init() {
-	console.log('Make something awesome!');
+	function typeWriter(progress) {
+		const txt= 'What text prediction teaches us about language'
+		const txt_progress = Math.max(0, Math.min(txt.length + 1, Math.floor(progress * txt.length)))
+		const hed = d3.select('.intro__hed')
+
+		console.log(hed.text())
+
+		hed.text(txt.substring(0, txt_progress))
+	}
+
+	let progress = 0
+	d3.interval(function() {
+		progress += 0.01
+		typeWriter(progress)
+	}, 50)
 }
 
 export default { init, resize };
