acb84c6abab8d84fd429ba25faacf691be013b96
Jan Diehm <jandiehm@Jans-MacBook-Pro-2.local>
2019-02-22 16:28:53 -0600

    styled up and mobile mostly working (check issues)

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -3,21 +3,36 @@
 function resize() {}
 
 function init() {
-	function typeWriter(progress) {
-		const txt= 'What text prediction teaches us about language'
-		const txt_progress = Math.max(0, Math.min(txt.length + 1, Math.floor(progress * txt.length)))
-		const hed = d3.select('.intro__hed')
+	const hed = d3.select('.intro__typing')
+			const txt= 'What text prediction teaches us about language'
 
-		console.log(hed.text())
+	function typeWriter(progress) {
+		const txt_progress = Math.max(0, Math.min(txt.length + 1, Math.floor(progress * txt.length)))
 
 		hed.text(txt.substring(0, txt_progress))
 	}
 
-	let progress = 0
-	d3.interval(function() {
-		progress += 0.01
-		typeWriter(progress)
-	}, 50)
+	function delayTyping() {
+		let progress = 0
+		let interval = d3.interval(function() {
+			if (hed.text().length == txt.length) {
+				interval.stop();
+				d3.select('.blink')
+					.transition()
+						.delay(5000)
+						.style('animation', 'none')
+						.style('border-right', 'none')
+				return;
+			} else {
+				progress += 0.01
+				typeWriter(progress)
+			}
+		}, 50)
+	}
+
+	setTimeout(delayTyping, 1000);
+
+	window.addEventListener('scroll', function() { d3.select('.more-icon').style('display', 'none') });
 }
 
 export default { init, resize };
